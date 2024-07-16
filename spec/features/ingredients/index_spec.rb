61c2af39f48a20ea0e54a1269d47c9902d434c43
Lance Nelson <lancen04@gmail.com>
2024-07-16 17:32:55 -0400

    completed user story 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe "the ingredients index" do
+  before(:each) do
+    @ingredient1 = Ingredient.create!(name: "Salt", cost: 2)
+    @ingredient2 = Ingredient.create!(name: "Pepper", cost: 3)
+    @ingredient3 = Ingredient.create!(name: "Olive Oil", cost: 5)
+
+  end
+  
+  # User Story 1
+  it "displays a list of all ingredients including their name and cost" do 
+    visit "/ingredients"
+    save_and_open_page
+    within("#ingredient-#{@ingredient1.id}") do
+      expect(page).to have_content("#{@ingredient1.name}: $#{@ingredient1.cost}")
+    end
+
+    within("#ingredient-#{@ingredient2.id}") do
+      expect(page).to have_content("#{@ingredient2.name}: $#{@ingredient2.cost}")
+    end
+
+    within("#ingredient-#{@ingredient3.id}") do
+      expect(page).to have_content("#{@ingredient3.name}: $#{@ingredient3.cost}")
+    end
+  end
+end
